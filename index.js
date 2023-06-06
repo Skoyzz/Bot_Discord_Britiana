64b75f1518d4b1bf59fdee447db4ec8f479119e3
Skoyzz <91893815+Skoyzz@users.noreply.github.com>
2023-06-06 18:12:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, Intents, GatewayIntentBits } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
+const ffmpegPath = require('ffmpeg-static').path;
 
 const client = new Client({
     intents: [
@@ -10,30 +13,84 @@ const client = new Client({
 });
 
 const cmdsk2000 = new SlashCommandBuilder()
-    .setName("k2000")
-    .setDescription("Fait la commande pour le savoir :)");
+    .setName('k2000')
+    .setDescription('Fait la commande pour le savoir :)');
 
 const pingpong = new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Fait la commande pour le savoir :)");
+    .setName('ping')
+    .setDescription('Fait la commande pour le savoir :)');
 
-client.on("ready", () => {
-    client.application.commands.set([cmdsk2000.toJSON(), pingpong.toJSON()]);
-    console.log("Bot OK !");
+const minecraft = new SlashCommandBuilder()
+    .setName('minecraft')
+    .setDescription("Pour avoir l'IP sur serveur Minecraft Britania");
+
+const play = new SlashCommandBuilder()
+    .setName('play')
+    .setDescription('Ne pas utiliser en cours de développement')
+    .addStringOption(option =>
+        option.setName('url')
+            .setDescription('Ne pas utiliser en cours de développement - URL de la vidéo YouTube')
+            .setRequired(true));
+
+client.once('ready', () => {
+    client.guilds.cache.forEach(guild => {
+        guild.commands.set([cmdsk2000.toJSON(), pingpong.toJSON(), minecraft.toJSON(), play.toJSON()])
+            .then(() => {
+                console.log(`Slash commands registered for guild "${guild.name}" (${guild.id})`);
+            })
+            .catch(console.error);
+    });
 });
 
-client.on("messageCreate", message => {
-    console.log(message);
-});
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
 
-client.on("interactionCreate", interaction => {
-    if (interaction.isCommand()) {
-        if (interaction.commandName === "k2000") {
-            interaction.reply("En cours de développement");
-        } else if (interaction.commandName === "ping") {
-            interaction.reply("En cours");
+    const { commandName, options } = interaction;
+
+    if (commandName === 'k2000') {
+        interaction.reply('<@440189962638327828>... Que dire sur ce monsieur, On peut dire qu\'il est toujours derrière le Monsieur <@256893525768011776> et c\'est pas le seul, il est aussi derrière <@263686495167184906> ' + 'https://media.tenor.com/o3AN6z7vah8AAAAC/vilebrequin-vilebrequin-merde.gif');
+    } else if (commandName === 'ping') {
+        interaction.reply('Pong!');
+    } else if (commandName === 'minecraft') {
+        interaction.reply('Salut ! L\'IP du serveur Britania est : 000.000.000.000');
+    } else if (commandName === 'play') {
+        const url = options.getString('url');
+        if (!url) {
+            interaction.reply('Veuillez fournir une URL de vidéo YouTube valide !');
+            return;
+        }
+
+        const voiceChannel = interaction.member.voice.channel;
+        if (!voiceChannel) {
+            interaction.reply('Vous devez rejoindre un salon vocal d\'abord !');
+            return;
+        }
+
+        try {
+            const connection = joinVoiceChannel({
+                channelId: voiceChannel.id,
+                guildId: voiceChannel.guild.id,
+                adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+                ffmpeg: ffmpegPath
+            });
+
+            const stream = ytdl(url, { filter: 'audioonly' });
+            const resource = createAudioResource(stream);
+            const player = createAudioPlayer();
+
+            player.play(resource);
+            connection.subscribe(player);
+
+            player.on(AudioPlayerStatus.Idle, () => {
+                connection.destroy();
+            });
+
+            interaction.reply('La lecture de la musique a commencé !');
+        } catch (error) {
+            console.error(error);
+            interaction.reply('Une erreur s\'est produite lors de la lecture de la musique.');
         }
     }
 });
 
-client.login("_____token______");
+client.login("mettre_le_token_du_bot");
